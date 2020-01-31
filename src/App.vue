3c812f9bb92da7daf70e4b4db2c742e3af7d7206
kickbuttowski80 <pazpaz25@gmail.com>
2020-01-30 19:04:39 -0800

    fsetup navbar and navigation-drawer

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app >
+     <navbar-app></navbar-app>
+     <main></main>
+  </v-app>
+</template>
+
+<script>
+ 
+import Navbar from './components/Navbar'
+export default {
+  name: 'App',
+
+  components: {
+    'navbar-app': Navbar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-sheet
+      height="100%"
+    >
+    <v-app-bar>
+      <v-app-bar-nav-icon  @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text >
+        <v-icon left>supervisor_account</v-icon>
+        View Meetups
+      </v-btn>  
+    </v-app-bar>    
+    <v-navigation-drawer v-model="drawer">         
+    <v-list>
+    <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        link
+    >
+        <v-list-item-icon>
+        <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+    </v-list-item>
+    </v-list>    
+    </v-navigation-drawer>     
+  </v-sheet>
+</template>
+<script>
+export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'View Meetups', icon: 'supervisor_account' },
+          { title: 'About', icon: 'question_answer' },
+        ],
+      }
+    },
+  }
+</script>
