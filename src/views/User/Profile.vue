2e0731d2e639e7631abfa640b407eaff8212589d
kickbuttowski80 <pazpaz25@gmail.com>
2020-02-23 12:17:07 -0800

    added the email info and how many meetups an user have created to the profile page and change the title from User Page to owner name profile

--- src/views/User/Profile.vue
+++ src/views/User/Profile.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
-        <h1>User Page</h1>
+        <h1>{{owner_name}} profile</h1>
         <h2>Name: {{owner_name}}</h2>
+        <h2>Email: {{user_info.email}}</h2>
+        <ul v-for="(meetup,i) in meetups" v-bind:key="i">
+            <li>{{meetup.title}}</li>
+        </ul>
+        
     </div>
 </template>
 <script>
@@ -9,8 +14,13 @@ import {mapState} from 'vuex'
 export default {
     computed: {
         ...mapState({
-             owner_name: state => state.user.displayName 
-        })
+             owner_name: state => state.user.displayName,
+             user_info: state => state.user
+        }),
+        meetups(){
+           return this.$store.getters.loadedMeetups
+                        .filter( meetup => meetup.creatorId === this.$store.getters.user.uid )
+        } 
     }
 }
 </script>
