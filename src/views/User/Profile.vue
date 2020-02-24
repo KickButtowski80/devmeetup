6c1cfcaf35db9a7f356a89a59661b3b85cc1ca77
kickbuttowski80 <pazpaz25@gmail.com>
2020-02-24 11:40:54 -0800

    correcting numbering list of meetups that current user created

--- src/views/User/Profile.vue
+++ src/views/User/Profile.vue
@@ -1,13 +1,50 @@
-<template>
-    <div>
-        <h1>{{owner_name}} profile</h1>
-        <h2>Name: {{owner_name}}</h2>
-        <h2>Email: {{user_info.email}}</h2>
-        <ul v-for="(meetup,i) in meetups" v-bind:key="i">
-            <li>{{meetup.title}}</li>
-        </ul>
-        
-    </div>
+<template  >
+  <v-card
+    class="mx-auto"
+    max-width="434"
+    
+    tile
+  >
+   <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    > 
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">Name: {{owner_name}}</v-list-item-title>
+              <v-list-item-subtitle>Email: {{user_info.email}}</v-list-item-subtitle>
+              <v-list-item-subtitle>Meetup organizer :</v-list-item-subtitle>
+                <ol start="1" v-for="(meetup,i) in meetups" v-bind:key="i">
+                    <span >{{i+1}}. {{meetup.title}}</span>
+                </ol>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
 </template>
 <script>
 import {mapState} from 'vuex' 
