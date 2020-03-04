5534a14a7ec887258616da3f554486b14963236d
kickbuttowski80 <pazpaz25@gmail.com>
2020-03-03 18:47:59 -0800

    remove vuex-orm for now. Added list of Meetup Registred list to an user profile

--- src/views/User/Profile.vue
+++ src/views/User/Profile.vue
@@ -35,17 +35,34 @@
             <v-list-item-content>
               <v-list-item-title class="title">Name: {{owner_name}}</v-list-item-title>
               <v-list-item-subtitle>Email: {{user_info.email}}</v-list-item-subtitle>
-              <v-list-item-subtitle>Meetup organizer :</v-list-item-subtitle>
-                <ol start="1" v-for="(meetup,i) in meetups" v-bind:key="i">
-                    <span >{{i+1}}. {{meetup.title}}</span>
-                </ol>
+              <template v-if="meetups.length> 0 ">
+                <v-list-item-subtitle>Meetup organizer :</v-list-item-subtitle>
+                  <v-card color="rgba(255, 0, 0, 0.5)">
+                    <ol start="1" v-for="(meetup,i) in meetups" v-bind:key="i">
+                        <span >{{i+1}}. {{meetup.title}}</span>
+                    </ol>              
+                </v-card>
+              </template>
+              <template>
+                <div>
+                  <v-list-item-subtitle>Meetup Registred :</v-list-item-subtitle>
+                  <v-card color="rgba(255, 0, 0, 0.5)">
+                    <ol start="1" v-for="(meetup,i) in registeredMeetups" v-bind:key="i">
+                        <span >{{i+1}}. {{meetup.title}}</span>
+                    </ol>              
+                  </v-card>
+                </div>
+              </template>
+
             </v-list-item-content>
           </v-list-item>
         </v-col>
       </v-row>
     </v-img>
   </v-card>
-  <h1>profile info: {{this.profilesInfo}}</h1>
+  <!--<h3>orginzer meetups:  {{this.meetups}}</h3>
+  <h3>registered meetups: {{this.registeredMeetups}}</h3>
+  <h3>All the meetups: {{this.$store.getters.loadedMeetups}} </h3> -->
   </div>
 </template>
 <script>
@@ -56,15 +73,32 @@ export default {
              owner_name: state => state.user.displayName,
              user_info: state => state.user
         }),
+        registeredMeetups(){
+          let rm= this.$store.getters.currentUserProfileInfo.registeredMeetups
+          let allm = this.$store.getters.loadedMeetups
+          let meetupsInfo = []
+          let i , j 
+          console.log("rm and all meetups are " + JSON.stringify(allm))
+          for (i = 0; i < rm.length; i++) {
+            console.log("rm=" + rm[i].toString()  )
+            for ( j = 0 ; j < allm.length; j++){
+              console.log("lm= " + JSON.stringify(allm[j]))
+                if(allm[j].id == rm[i].toString()) 
+                  meetupsInfo.push(allm[j])
+                  }
+          }
+          console.log("meetupsInfo " + JSON.stringify(meetupsInfo))
+          return meetupsInfo
+        },
         meetups(){
            return this.$store.getters.loadedMeetups
                        .filter( meetup => meetup.creatorId === this.$store.getters.user.uid )
         },
         profilesInfo(){
-           let currentUserProfile = this.$store.state.profilesInfo
-                          .find( userProfile =>
-                                 userProfile.id === this.$store.getters.user.uid )
-           return currentUserProfile         
+          //  let currentUserProfile = this.$store.state.profilesInfo
+          //                 .find( userProfile =>
+          //                        userProfile.id === this.$store.getters.user.uid )
+           return this.$store.getters.currentUserProfileInfo       
         }
 
                          
